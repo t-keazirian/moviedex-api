1226c9a5881480233ed9d63dec9a8f1d4467ea2e
Taylor Keazirian <taylork26@gmail.com>
2021-02-26 13:55:30 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIEDEX = require('./moviedex.json');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+	const apiToken = process.env.API_TOKEN;
+	const authToken = req.get('Authorization');
+
+	if (!authToken || authToken.split(' ')[1] !== apiToken) {
+		return res.status(401).json({ error: 'unauthorized request' });
+	}
+	next();
+});
+
+function handleGetMovies(req, res) {
+	const { genre = '', country = '', avg_vote = '' } = req.query;
+	let response = MOVIEDEX;
+
+	if (genre) {
+		response = response.filter((movie) =>
+			movie.genre.toLowerCase().includes(genre.toLowerCase())
+		);
+	}
+
+	if (country) {
+		response = response.filter((movie) =>
+			movie.country.toLowerCase().includes(country.toLowerCase())
+		);
+	}
+
+  if (avg_vote) {
+    response = response.filter(movie => movie.avg_vote >= avg_vote)
+    }
+
+	res.json(response);
+	
+}
+
+app.get('/movie', handleGetMovies);
+
+app.listen(8000, () => {
+	console.log('Server listening on PORT 8000');
+});
